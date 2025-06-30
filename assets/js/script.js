242295cfc74206b679c2f85906e690eaad40b4f2
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-30 16:16:58 +0200

    maj css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@ let nbPhoto = 16
 for (i = 0; i < nbPhoto; i++) {
     hauteur = (Math.floor(Math.random() * 2))+2;
     largeur = (Math.floor(Math.random() * 2))+2;
-    document.getElementById("galerie").innerHTML += `<div class="mx-auto my-2 photo"><a href="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}" data-lightbox="galerie" data-title="Photo par Claire Montfort"><img src="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}"></a></div>`
+    document.getElementById("galerie").innerHTML += `<div class="mx-auto my-2 photo"><a href="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}" data-lightbox="galerie" data-title="Photo par Claire Montfort" class="zoomIn"><img src="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}"></a></div>`
 }
