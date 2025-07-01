f40652b0a5e7fc2325264100e56fe07070ba522e
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-07-01 09:18:48 +0200

    Maj id random img

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,6 @@ let nbPhoto = 16
 for (i = 0; i < nbPhoto; i++) {
     hauteur = (Math.floor(Math.random() * 2))+2;
     largeur = (Math.floor(Math.random() * 2))+2;
-    document.getElementById("galerie").innerHTML += `<div class="mx-auto my-2 photo"><a href="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}" data-lightbox="galerie" data-title="Photo par Claire Montfort" class="zoomIn"><img src="https://picsum.photos/${hauteur}00/${largeur}00?random=${i}"></a></div>`
+    random = (Math.floor(Math.random() * 600))+2;
+    document.getElementById("galerie").innerHTML += `<div class="mx-auto my-2 photo"><a href="https://picsum.photos/id/${random}/1000/1000" data-lightbox="galerie" data-title="Photo par Claire Montfort" class="zoomOut"><img src="https://picsum.photos/id/${random}/${hauteur}00/${largeur}00"></a></div>`
 }
